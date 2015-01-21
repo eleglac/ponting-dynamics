bcfea976e710043e7000a5070aa2bcc1c4595426
Rob Nagle <rjn945@gmail.com>
2015-01-21 14:50:35 -0800

    Base page using window-based sizing and flex CSS... prolly not too compatible with things

--- project.clj
+++ project.clj
@@ -5,10 +5,25 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [hiccup "1.0.5"]
+                 [org.clojure/clojurescript "0.0-2069"]
+                 [prismatic/om-tools "0.3.10"]                 
                  ]
-  :plugins [[lein-ring "0.8.13"]]
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.0"]
+            ]
+
   :ring {:handler ponting-dynamics.core.handler/app}
+  
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+  
+  :cljsbuild {:builds
+               [{:source-paths ["src/cljs"]
+                 :compiler {:output-to "resources/public/js/generic.js"
+                            :optimizations :whitespace
+                            :pretty-print  true}}]}
+  )
---
+++ src/cljs/ponting_dynamics/connect.cljs
@@ -0,0 +1,4 @@
+(ns ponting-dynamics.connect
+  (:require [clojure.browser.repl :as repl]))
+
+(repl/connect "http://localhost:9000/repl")
