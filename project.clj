dc42151845b03658c71730abb64749cff45b9ca4
Rob Nagle <rjn945@gmail.com>
2015-01-23 14:31:20 -0800

    Version to 0.3-alpha, cleaning up project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject ponting-dynamics "0.2-alpha"
+(defproject ponting-dynamics "0.3-alpha"
   :description "A personal website for Alex J. Ponting."
   :url "http://www.pontingdynamics.org"
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [ring/ring-defaults "0.1.2"]
+  :dependencies [[compojure "1.3.1"]
+                 [hiccup "1.0.5"]  
+                 [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2069"]
-                 [hiccup "1.0.5"]
-                 ]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-defaults "0.1.2"]]
 
   :source-paths ["src/clj" "src/cljs"]
 
@@ -22,10 +21,7 @@
   :profiles
     {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                           [ring-mock "0.1.5"]]}
-     :uberjar {:aot :all} ;;because otherwise Leiningen is concerned for my well-being
-      
-     }
-
+     :uberjar {:aot :all}} ;; otherwise Leiningen is concerned for my well-being
 
   :ring {:handler ponting-dynamics.application/app} 
   
